69952770213db1afbc2cd8c6f40396f1ca7f3aea
zdanowiczkonrad <zdanowicz.konrad@gmail.com>
2017-03-12 17:59:15 +0100

    Added decorators support to Babel presets

--- src/routes/Quotes/quotes.js
+++ src/routes/Quotes/quotes.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux'
 import React from 'react'
 
-
+@connect(state => ({ quotes: state.quotes }))
 export default class Quotes extends React.Component {
 
     render() {
-        return <div>Hello</div>;
+        console.log(this.props);
+        return <div>{JSON.stringify(this.props.quotes)}</div>;
     }
 }
---
+++ src/routes/Quotes/reducer.js
@@ -0,0 +1,8 @@
+import { quotes } from '../../../../../quotes.json';
+
+const initialState = {
+    quotes: []
+}
+export default function reducer(state = initialState, action) {
+    return state;
+}
